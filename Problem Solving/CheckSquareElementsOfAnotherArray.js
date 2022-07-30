fdf9dd9865c823699c34ddf6a427fec0d239b3d8
Pujari Shiv Kumar <pujari.shiv98@gmail.com>
2022-07-31 00:53:32 +0530

    More optimized solution

--- Problem Solving/CheckSquareElementsOfAnotherArray.js
+++ Problem Solving/CheckSquareElementsOfAnotherArray.js
@@ -23,5 +23,37 @@ function CheckSquareElementsofAnotherArray(arr1,arr2){
     return true;
 }
 
-const result = CheckSquareElementsofAnotherArray(arr1,arr2);
+// arr1 = [1,2,3,4] arr2 = [1,4,9,16] 
+// map1 = {1:1, 2:1, 3:1, 4:1 }
+// map2 = {1:1, 4:1, 9:1, 16:1}
+// compare 
+
+
+function OptimisedVersion(arr1,arr2){
+    let map1 = {};
+    let map2 = {};
+
+    for (let i=0; i<arr1.length; i++){
+        map1[arr1[i]] = (map1[arr1[i]] || 0) + 1;
+    }    
+
+    for(let i=0; i<arr2.length; i++){
+        map2[arr2[i]] = (map2[arr2[i]] || 0) + 1;
+    }
+
+    for(let key in map1){
+        if(!map2[key * key]){
+            return false;
+        }
+
+        if(map1[key] !== map2[key * key]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+//const result = CheckSquareElementsofAnotherArray(arr1,arr2);
+const result = OptimisedVersion(arr1,arr2);
 console.log(result)
